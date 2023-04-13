de39b4349ff128de309ed589ae4ca685ce2d9163
daniel deflores <deflores@udel.edu>
2023-04-13 17:25:13 -0400

    set up basic template for files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function App(): JSX.Element {
-    return ();
+    return (
+        <div>
+            <div>CISC-275 Final Project</div>
+            <div>By Dan, Jess, and James</div>
+        </div>
+    );
 }
 
 export default App;
