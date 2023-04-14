ed07cbe1a886e203d80a38cd145306313bc4cd52
Daniel DeFlores <ddeflores2@gmail.com>
2023-04-14 14:25:41 -0400

    add personal access key for environment

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <div>CISC275 Final Project</div>
-            <div>By Dan, Jess, and James</div>
+            <div>By: Dan, Jess, and James</div>
         </div>
     );
 }
